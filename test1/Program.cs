0ae70c2cd6ef732be44c280f58c6ae377a45e4ae
sergey877 <sergei1976-19.76@mail.ru>
2024-03-06 22:38:44 +0500

    added first

---
+++ test1/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array ={2,3,4,5,6,7,1};
+int n=7;
+int i=0;
+int sum =0;
+
+while(i<n)
+{
+    if (array[i]>3)
+    {
+        sum=sum+array[i];
+    }
+    i=i+1;
+}
+Console.WriteLine(sum);
